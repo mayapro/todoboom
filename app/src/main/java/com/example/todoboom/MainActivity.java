b778227995965be7c10f9db2a8ebf1093a730860
maya.pro <Maya.Prozan@mail.huji.ac.il>
2020-05-05 19:38:03 +0300

    done with ex2

--- app/src/main/java/com/example/todoboom/MainActivity.java
+++ app/src/main/java/com/example/todoboom/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-//import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import java.util.ArrayList;
